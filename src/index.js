073be0e3d4f2c67bed7f4ee232ccf44d59d40247
Jose V Sebastian <josevseb99@gmail.com>
2021-10-04 00:38:09 +0530

    Completed challenge

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-ReactDOM.render(<h1>Hello World!</h1>, document.getElementById("root"));
+const fName = "Jose";
+const lName = "V Sebastian";
+const luckyNumber = Math.floor(Math.random() * 10);
+
+ReactDOM.render(
+  <div>
+    <h1>
+      Hello {fName} {lName}!
+    </h1>
+    <p>My lucky number is {luckyNumber}</p>
+  </div>,
+  document.getElementById("root")
+);
